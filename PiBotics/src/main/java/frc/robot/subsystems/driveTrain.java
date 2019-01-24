b1fa9b8b9c43f4ead07b3a74d943396be0f01dbc
Xavier Veselovec <xvraiden@gmail.com>
2019-01-23 19:39:28 -0500

    We finished the drive system and need to test

--- PiBotics/src/main/java/frc/robot/subsystems/driveTrain.java
+++ PiBotics/src/main/java/frc/robot/subsystems/driveTrain.java
@@ -8,17 +8,49 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+import com.ctre.phoenix.motorcontrol.can.*;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import frc.robot.commands.driveJoy;
 
 /**
  * An example subsystem.  You can replace me with your own Subsystem.
  */
-public class ExampleSubsystem extends Subsystem {
+public class driveTrain extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
+
+  WPI_TalonSRX lf;
+  WPI_TalonSRX lr;
+  WPI_TalonSRX rf;
+  WPI_TalonSRX rr;
+
+  SpeedControllerGroup left;
+  SpeedControllerGroup right;
+
+  DifferentialDrive piboticsDrive;
+
+  public driveTrain() {
+    lf = new WPI_TalonSRX(0);
+    lr = new WPI_TalonSRX(1);
+    rf = new WPI_TalonSRX(2);
+    rr = new WPI_TalonSRX(3);
+
+    left = new SpeedControllerGroup(lf,lr);
+    right = new SpeedControllerGroup(rf,rr);
+    
+		piboticsDrive = new DifferentialDrive(left, right);
+	}
+
+	public void arcadeDrive(double x, double y) {
+		piboticsDrive.arcadeDrive(x, y);
+	}
 
   @Override
   public void initDefaultCommand() {
     // Set the default command for a subsystem here.
     // setDefaultCommand(new MySpecialCommand());
+    setDefaultCommand(new driveJoy());
   }
 }
