0d8a945660f8ef8a707dc9f6f4a85b725403bb4f
Xavier Veselovec <xvraiden@gmail.com>
2019-01-28 19:48:40 -0500

    Fixed the green errors

--- PiBotics/src/main/java/frc/robot/subsystems/BallLiftTalon.java
+++ PiBotics/src/main/java/frc/robot/subsystems/BallLiftTalon.java
@@ -7,8 +7,7 @@
 
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.*;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
